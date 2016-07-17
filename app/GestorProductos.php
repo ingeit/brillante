706043185dde531cf6ae2d28248b3b40f6ea97e8
rickybruno <ricardobruno_89@hotmail.com>
2016-07-16 21:57:50 -0300

    soy ricky

--- app/GestorProductos.php
+++ app/GestorProductos.php
@@ -11,6 +11,7 @@ use Illuminate\Pagination\LengthAwarePaginator;
 class GestorProductos extends Model
 {
     //soy ricky
+    //tu vieja
     public function alta(Productos $p)
     {
         $params = array(
