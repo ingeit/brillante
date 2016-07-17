9b0d86bb2eaf80263d760e2a944774f573682456
rickybruno <ricardobruno_89@hotmail.com>
2016-07-17 15:01:43 -0300

    Funcion nueva venta (parametros solo de fecha(varchar) monto(double)

--- app/GestorVentas.php
+++ app/GestorVentas.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class GestorVentas extends Model
 {
-    //probando source tree
+    public function nueva(Ventas $v)
+    {
+        $params = array(
+            $v->fecha,
+            $v->monto);
+        
+        
+    	$result = DB::select('call venta_nueva(?,?)',$params);
+        return $result;
+    }
 }
--- app/Ventas.php
+++ app/Ventas.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ventas extends Model
 {
-    //
+    public $vIdVenta;
+    public $vFecha;
+    public $vMonto;
+    
+    public $timestamps = false; 
+      
+    function __construct($fecha, $monto)
+    {
+        $this->vFecha = $fecha;
+        $this->vMonto = $monto;
+    }
+    
+    protected $fillable = [
+        'fecha', 'monto',
+    ];
+   
+     public function dame($id){
+        $result = DB::select('call venta_dame(?)',array($id));
+        //el foreach me ayuda a pasar la consulta estructurada de store procedure
+        // a objeto, de esa manera se puede trabajar mas facil y autocompleta los campos
+        // gracias al form::model , el objeto es un Model de la base de datos por eso 
+        // uso fill() para completarlo.
+        foreach ($result as $r) {
+                $this->fill([
+                "fecha" => $r->fecha,
+                "monto" => $r->monto,
+            ]);
+                $this->vIdVenta = $r->idVenta;
+        }
+    }
+
 }
