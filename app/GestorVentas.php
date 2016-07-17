b6954f7bbd571ef613b8442e979c4dc5c0f59957
rickybruno <ricardobruno_89@hotmail.com>
2016-07-17 15:35:51 -0300

    Funcion nueva venta (parametros solo de fecha(varchar) monto(double) Correcion source tree puto

--- app/GestorVentas.php
+++ app/GestorVentas.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class GestorVentas extends Model
 {
-<<<<<<< HEAD
+    
     public function nueva(Ventas $v)
     {
         $params = array(
@@ -17,7 +17,4 @@ class GestorVentas extends Model
     	$result = DB::select('call venta_nueva(?,?)',$params);
         return $result;
     }
-=======
-    //probando source tree
->>>>>>> parent of 9b0d86b... Funcion nueva venta (parametros solo de fecha(varchar) monto(double)
 }
