f343e220384eb131a81e9c1381b468b9acd373dd
rickybruno <ricardobruno_89@hotmail.com>
2016-09-05 13:39:51 -0300

    Correcciones esteticas, y terminado baja prducto

--- resources/views/productos/form.blade.php
+++ resources/views/productos/form.blade.php
@@ -13,9 +13,6 @@
                     <div class="panel-heading">{{Session::get('titulo')}}</div>
                     <div class="panel-body">
                         @if(Session::has('edicion'))
-                            {!! Form::open(['route' => ['productos.destroy',$data->pIdProducto],'method'=>'delete']) !!}
-                                    <button style="float:right" class="btn btn-danger" type="submit">Eliminar</button>
-                            {!! Form::close() !!}
                             {!! Form::model($data,['route' => ['productos.update',$data->pIdProducto],'method'=>'PUT']) !!}
                         @else
                             {!! Form::open(array('route' => 'productos.store')) !!}
@@ -53,6 +50,11 @@
                                 </div>
                             </div>
                             <button type="submit" style="float:right" class="btn btn-default">{{Session::get('boton')}}</button>
+                            @if(Session::has('edicion'))
+                                {!! Form::open(['route' => ['productos.destroy',$data->pIdProducto],'method'=>'delete']) !!}
+                                        <button style="float:right" class="btn btn-danger" type="submit">Eliminar</button>
+                                {!! Form::close() !!}
+                            @endif
                         {!! Form::close() !!}
                     </div>
                 </div>
