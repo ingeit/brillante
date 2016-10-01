cf32a733c6c1c178e82d59a5d80f61751e089a78
Ricky B <Ricky B@Ricky>
2016-10-01 16:55:10 -0300

    Sin materialize

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,33 +7,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    
-                <div class="card small col-md-4">
-                   <div class="card-image waves-effect waves-block waves-light">
-                     <img class="activator" src="{{ asset('assets/imagenes/venta.jpg') }}">
-                   </div>
-                   <div class="card-content">
-                     <span class="card-title activator grey-text text-darken-4">VENTAS<i class="material-icons right">more_vert</i></span>
-                   </div>
-                   <div class="card-reveal">
-                     <span class="card-title grey-text text-darken-4">Elija una opcion<i class="material-icons right">close</i></span>
-                     <button><a href="{{ route('ventas.index',['seccion'=>'index']) }}">Nueva Venta</a></button>
-                   </div>
-                 </div>
-                    
-                    <div class="card small col-md-4">
-                   <div class="card-image waves-effect waves-block waves-light">
-                     <img class="activator" src="{{ asset('assets/imagenes/venta.jpg') }}">
-                   </div>
-                   <div class="card-content">
-                     <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-                     <p><a href="#">This is a link</a></p>
-                   </div>
-                   <div class="card-reveal">
-                     <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                     <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                   </div>
-                 </div>
+             
                     
                 </div>
             </div>
