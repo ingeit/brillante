f0903a8980a770b1e72722edbe53a35a823375f7
Ricky <Ricky@Equipo-Ricky>
2016-10-12 09:27:58 -0300

    mira vos

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -11,7 +11,7 @@ class Admin
 {
     /**
      * Handle an incoming request.
-     *
+     *Mira vos
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
      * @return mixed
