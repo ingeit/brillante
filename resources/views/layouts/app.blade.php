8af4858c0072a260604a902f8e4b884a5a4cbc83
Ricky <Ricky@Equipo-Ricky>
2016-10-14 13:08:36 -0300

    Usuarios auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,8 +58,11 @@ header("Expires: 0 "); // Proxies. ?>
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Home</a></li>
+                    
                     <li><a href="{{ route('productos.index') }}">Productos</a></li>
-                    <li><a href="{{ route('proveedores.index') }}">Proveedores</a></li>
+                    @if(Auth::user()->role === 'admin')
+                        <li><a href="{{ route('proveedores.index') }}">Proveedores</a></li>
+                    @endif
                     <li><a href="{{ route('ventas.index') }}">Ventas</a></li>
                     <li><a href="{{ route('compras.index') }}">Compras</a></li>
                     <li><a href="{{ route('ingresos.index') }}">Ingreso Local</a></li>
@@ -68,10 +71,12 @@ header("Expires: 0 "); // Proxies. ?>
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Acceder</a></li>
-                        <li><a href="{{ url('/register') }}">Registrarse</a></li>
-                    @else
+
+                    <!--<li><a href="{{ url('/login') }}">Acceder</a></li>-->
+                     @if (Auth::user()->role === 'admin')
+                        <li><a href="{{ url('/register') }}">Registrar nuevo usuario</a></li>
+                     @endif   
+                    
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
@@ -81,7 +86,6 @@ header("Expires: 0 "); // Proxies. ?>
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Salir</a></li>
                             </ul>
                         </li>
-                    @endif
                 </ul>
             </div>
         </div>
