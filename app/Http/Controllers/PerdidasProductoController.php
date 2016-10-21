d67a137950a66a275d7369065947b4b4e0c4f67a
Ricky <Ricky@Equipo-Ricky>
2016-10-21 13:06:28 -0300

    Solucionado Perdidas Productos!

--- app/Http/Controllers/PerdidasProductoController.php
+++ app/Http/Controllers/PerdidasProductoController.php
@@ -77,8 +77,8 @@ class PerdidasProductoController extends Controller
         
         //Ahora q tenemos el monto fecha (para venta), tenemos el contac de las lineas
         //Generamos la venta y a su vez las lineas ventas en un solo SP
-        $params = array($cadena);
-    	$resultado = DB::select('call perdida_producto_nuevo(?)',$params);
+        $params = array($cadena,'p','@codigo','@mensaje');
+    	$resultado=DB::select('call perdida_producto_nuevo(?,?,?,?)',$params);
         return $resultado;
     }
 
