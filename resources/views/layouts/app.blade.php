f9195acd33bd3c1c7e9b22c3112794013462919f
MasterK <MasterK@MasterKNet>
2016-10-24 15:02:01 -0300

    js venta funcionando

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -134,12 +134,14 @@ header("Expires: 0 "); // Proxies. ?>
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+    @yield('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
     <script src="/js/js-query/jquery-ui.min.js"></script>
-    @yield('scripts')
-    <!-- Compiled and minified JavaScript -->
     <script type="text/javascript" src="js/materialize.min.js"></script>
+    
+    <!-- Compiled and minified JavaScript -->
+    
     <script>$(document).ready(function(){
         $('.button-collapse').sideNav({
             menuWidth: 250, // Default is 240
